3ce5e9fc1ff688d04e45b355101a009b69101451
EriEri <erica.ff14.mail@gmail.com>
2021-11-15 04:04:31 +0900

    Add project files

---
+++ webpack.config.js
@@ -0,0 +1,65 @@
+const path = require('path');
+const TerserPlugin = require("terser-webpack-plugin");
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin')
+const webpack = require('webpack');
+
+const mode = process.env.NODE_ENV ?? 'development';
+const isDev = mode === 'development';
+
+module.exports = {
+    mode,
+    entry: {
+        dps: './src/component/dps/index.tsx',
+        settings: './src/component/settings/index.tsx'
+    },
+    output: {
+        filename: '[name].js',
+        path: path.resolve('overlay', 'public', 'js')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: [
+                    {
+                        loader: 'thread-loader',
+                        options: {
+                            workers: 2,
+                            workerParallelJobs: 50,
+                            workerNodeArgs: ['--max-old-space-size=512'],
+                            name: 'tsx-loader-pool',
+                        }
+                    },
+                    {
+                        loader: 'esbuild-loader',
+                        options: { loader: 'tsx', target: 'es2015' }
+                    }
+                ]
+            },
+        ]
+    },
+    optimization: {
+        minimize: true,
+        minimizer: [new TerserPlugin({
+            terserOptions: {
+                compress: isDev ? {} : { drop_console: true }
+            }
+        })],
+    },
+    plugins: [
+        new ForkTsCheckerWebpackPlugin(),
+        new webpack.ProvidePlugin({ React: "react" }),
+    ],
+    resolve: {
+        extensions: [".ts", ".tsx", ".js"],
+        fallback: {}
+    },
+
+    target: ["web", "es5"],
+    cache: {
+        type: 'filesystem',
+        buildDependencies: {
+            config: [__filename]
+        }
+    }
+};

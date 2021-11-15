be6209c3b0b4ee331a08f7409cbb1ba1a3baf6be
EriEri <erica.ff14.mail@gmail.com>
2021-11-16 02:52:28 +0900

    Fix wrong logging in "useLogger" hook

--- src/hooks/useLogger.tsx
+++ src/hooks/useLogger.tsx
@@ -1,18 +1,18 @@
 import { useContext, useEffect, useRef } from 'react';
 import { SettingsContext } from '../component/dps/index';
 
+export const defaultLogger = (m: any, ..._: any) => console.info(`%c[Pulse]: ${m}`, 'color: #6565ff');
+export const silentLogger = (..._: any) => { };
+export const selectLogger = (outputLogOnConsole: boolean) =>
+    outputLogOnConsole ? defaultLogger : silentLogger;
+
 export const useLogger = () => {
     const [settings] = useContext(SettingsContext);
 
-    const logger = useRef(settings.outputLogOnConsole
-        ? (m: any, ..._: any) => console.info(`%c[Pulse]: ${m}`, 'color: #6565ff')
-        : (..._: any) => { }
-    );
+    const logger = useRef(selectLogger(settings.outputLogOnConsole));
 
     useEffect(() => {
-        logger.current = settings.outputLogOnConsole
-            ? console.info
-            : (..._: any) => { };
+        logger.current = selectLogger(settings.outputLogOnConsole);
     }, [settings.outputLogOnConsole]);
 
     return { logger }

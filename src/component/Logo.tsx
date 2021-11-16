5afb44ad1b8883b5cb02e171ca7930d5b4d6ea42
EriEri <erica.ff14.mail@gmail.com>
2021-11-17 00:19:39 +0900

    Use external reference for logo svg

--- src/component/Logo.tsx
+++ src/component/Logo.tsx
@@ -9,12 +9,6 @@ export const LogoContainer: React.VFC<{}> = () => <>
 </>
 
 export const Logo: React.VFC<{}> = () =>
-    <svg className='padding' style={{ width: '28px', height: '28px' }} viewBox='0 0 50 50' xmlns='http://www.w3.org/2000/svg'>
-        <path
-            d='m2.6 25h10.9l6.5-15 10 30 6.5-15h10.9'
-            fill='none'
-            stroke='#ffffff'
-            strokeLinecap='round'
-            strokeWidth='5'
-        />
+    <svg className='padding' style={{ width: '28px', height: '28px' }}>
+        <use xlinkHref={`./public/img/logo.svg#logo`} />
     </svg>;

35f8ef9483eee3c5942e902e7ee60a6fc71ac135
EriEri <erica.ff14.mail@gmail.com>
2021-11-19 00:45:15 +0900

    Improve react-i18next types declaration

--- src/@types/react-i18next.d.ts
+++ src/@types/react-i18next.d.ts
@@ -1,9 +1,8 @@
-import { resources, defaultNS } from '../locales/i18n'
 import { LangResource } from './index'
 
 declare module 'react-i18next' {
     interface CustomTypeOptions {
-        defaultNS: typeof defaultNS;
+        defaultNS: keyof LangResource;
         resources: LangResource;
     };
 };

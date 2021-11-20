30f61d0db563cf01f4a6ef3876df91fc9641af9b
EriEri <erica.ff14.mail@gmail.com>
2021-11-20 22:14:55 +0900

    Add Translation files

---
+++ src/locales/en/message.ts
@@ -0,0 +1,5 @@
+export default {
+    turnOnSettingsMode: 'Unlock overlay to turn on settings mode',
+    turnOffSettingsMode: 'Lock overlay to turn off settings mode',
+    openSettingsWindow: 'Double-click to open settings window (required to disable click-through)',
+}

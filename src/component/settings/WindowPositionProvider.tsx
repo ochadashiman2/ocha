2b7342e1fc1cb8fb854c46940c2b6d4faffc8ad4
EriEri <erica.ff14.mail@gmail.com>
2021-11-29 22:29:14 +0900

    Add cleanup function

--- src/component/settings/WindowPositionProvider.tsx
+++ src/component/settings/WindowPositionProvider.tsx
@@ -3,8 +3,8 @@ import { saveWindowPosition } from "../../lib/Storage";
 
 export const WindowPositionProvider: React.VFC<{ children: JSX.Element }> = ({ children }) => {
     useEffect(() => {
-        window.setInterval(saveWindowPosition, 500);
-        return () => { }
+        const id = window.setInterval(saveWindowPosition, 500);
+        return () => window.clearInterval(id);
     }, []);
 
     return children;
